d53d88d20cd70bd9246eca0320ba052d73194fd5
CongLe0707 <thanhcong772003@gmail.com>
2025-05-30 09:47:29 +0700

    Them xem nguoi dung dang nhap va dang xuat luc nao và tong thoi dang nhap

--- src/main/java/com/example/Hdbank_project/controller/ApproverController.java
+++ src/main/java/com/example/Hdbank_project/controller/ApproverController.java
@@ -18,8 +18,6 @@ public class ApproverController {
     public ApproverController(UserSessionRepository userSessionRepository) {
         this.userSessionRepository = userSessionRepository;
     }
-
-    // Lấy lịch sử đăng nhập đăng xuất tất cả người dùng hoặc theo username
     @GetMapping("/sessions")
     public ResponseEntity<?> getUserSessions(@RequestParam(required = false) String username) {
         List<UserSession> sessions;
@@ -28,8 +26,6 @@ public class ApproverController {
         } else {
             sessions = userSessionRepository.findByUsername(username);
         }
-
-        // Có thể trả về trực tiếp hoặc map sang DTO để tránh lộ dữ liệu nhạy cảm
         return ResponseEntity.ok(sessions);
     }
 }
